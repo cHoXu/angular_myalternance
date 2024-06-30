6dab7eb9ca03ac34a2b78e9398ad39f8a4c5811f
Nicolas NAUDIN <nicolas@lbcd.fr>
2024-06-30 21:24:21 +0200

    maj admin

--- src/app/admin/user/admin-user-board/components/admin-user-offers/admin-user-offers.component.ts
+++ src/app/admin/user/admin-user-board/components/admin-user-offers/admin-user-offers.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {OfferComponent} from "../../../../../shared/components/offer/offer.component";
+import {OfferService} from "../../../../../core/formation/services/offer.service";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-admin-user-offers',
   standalone: true,
-    imports: [
-        OfferComponent
-    ],
+  imports: [
+    OfferComponent,
+    NgForOf
+  ],
   templateUrl: './admin-user-offers.component.html',
   styleUrl: './admin-user-offers.component.sass'
 })
 export class AdminUserOffersComponent {
-
+  offers = this.offersService.offers
+  constructor(private offersService: OfferService) {
+  }
 }
