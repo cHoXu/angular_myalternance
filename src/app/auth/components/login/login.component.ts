70555245e015ff08ec676a082be98462daa254ec
Nicolas NAUDIN <nicolas@lbcd.fr>
2024-07-02 17:50:05 +0200

    add company section

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {Location, NgClass} from "@angular/common";
+import {UniverseService} from "../../../shared/services/universe.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
-
+  universe!: string;
+  constructor(private universeService:UniverseService) {
+    this.universeService.universe$.subscribe((universe: string) => {
+      this.universe = universe;
+    });
+  }
 }
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {UniverseService} from "../../../shared/services/universe.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.sass'
 })
 export class RegisterComponent {
-
+  universe!: string;
+  constructor(private universeService:UniverseService) {
+    this.universeService.universe$.subscribe((universe: string) => {
+      this.universe = universe;
+    });
+  }
 }
