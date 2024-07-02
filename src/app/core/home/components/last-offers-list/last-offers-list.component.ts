ca0ff34065e77ea93ac29921022be6ac778aac64
Nicolas NAUDIN <nicolas@lbcd.fr>
2024-07-02 22:29:01 +0200

    add carousel

--- src/app/core/home/components/last-offers-list/last-offers-list.component.ts
+++ src/app/core/home/components/last-offers-list/last-offers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {OfferComponent} from "../../../../shared/components/offer/offer.component";
 import {OfferService} from "../../../formation/services/offer.service";
 import {Offer} from "../../../../shared/model/offer";
@@ -14,9 +14,14 @@ import {NgForOf} from "@angular/common";
   templateUrl: './last-offers-list.component.html',
   styleUrl: './last-offers-list.component.sass'
 })
-export class LastOffersListComponent {
+export class LastOffersListComponent implements AfterViewInit {
+  @ViewChild('offersScroll') offersScroll!: ElementRef;
   lastOffers!: Offer[];
   constructor(private offerService: OfferService) {
     this.lastOffers = this.offerService.findLatestOffers();
+  }
+
+  ngAfterViewInit(): void {
+    this.offersScroll.nativeElement.scrollLeft = 100;
   }
 }
